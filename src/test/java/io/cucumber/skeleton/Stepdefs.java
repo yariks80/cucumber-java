3224e0452a0c985ff0be95370e3e2f5f416a22b3
iaroslav.sviatkin <$viatk1N2013>
2019-05-23 01:57:44 +0300

    update

--- src/test/java/io/cucumber/skeleton/Stepdefs.java
+++ src/test/java/io/cucumber/skeleton/Stepdefs.java
@@ -1,11 +1,24 @@
 package io.cucumber.skeleton;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+
+    @When("^I wait (\\d+) hour$")
+    public void I_wait_hours(int hours) throws Throwable {
+        Thread.sleep(hours*0);
+    }
+
+    @Then("^my belly should growl$")
+    public boolean my_belly_should_growl() throws Throwable {
+        return true;
     }
 }
--- src/test/resources/io/cucumber/skeleton/belly.feature
+++ src/test/resources/io/cucumber/skeleton/belly.feature
@@ -1,6 +1,7 @@
-Feature: Belly
+Feature: a few cukes summary
 
-  Scenario: a few cukes
+	#a few cukes summary description
+  @TEST_MYC-3
     Given I have 42 cukes in my belly
     When I wait 1 hour
     Then my belly should growl
