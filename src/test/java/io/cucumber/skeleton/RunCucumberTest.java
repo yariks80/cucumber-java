4a55e76348a167a69180328d05c1e445cd29d618
iaroslav.sviatkin <$viatk1N2013>
2019-05-23 23:57:51 +0300

    update

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,19 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+
+        plugin = {
+                "json:target/reports.json",
+                "html:target/reports",
+                "pretty",
+        },
+
+        features = {"src\\test\\resources\\io\\cucumber\\skeleton"},
+        monochrome = true,
+        glue = {"io.cucumber.skeleton"},
+        tags = {"~@ignored"}
+)
+
 public class RunCucumberTest {
 }
